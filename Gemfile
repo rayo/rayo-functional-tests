8e46fdb1293154b9367c0cce02929853efb3ec81
Jason Goecke <jason@goecke.net>
2011-05-26 06:34:24 -0700

    Additional changes to ensure we may run this standalone

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source "http://rubygems.org"
+source "http://geminabox.voxeolabs.com/"
 
 group :development do
   gem 'rspec'
+  gem 'rake'
+  gem 'bundler'
+  gem 'rcov'
   gem 'rspec-tropo2'
   gem 'punchblock'
   gem 'awesome_print'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,18 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'timeout'
-require 'rspec'
-require 'yaml'
-require 'thread'
-require 'awesome_print'
-require 'drb'
-require 'punchblock'
-require 'rspec-tropo2'
+%w{
+  timeout
+  rspec
+  yaml
+  thread
+  awesome_print
+  drb
+  logger
+  rspec-tropo2
+  punchblock
+  }.each { |lib| require lib }
+  # /Users/jsgoecke/Dropbox/Development/punchblock/lib/punchblock
+  # /Users/jsgoecke/Dropbox/Development/rspec-tropo2/lib/rspec-tropo2
 
 Thread.abort_on_exception = true
 
