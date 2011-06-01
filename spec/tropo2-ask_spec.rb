6f30d0f7305e72f215bee2824f7c2968f665b739
Jason Goecke <jason@goecke.net>
2011-06-01 14:22:47 -0700

    Added additional ask tests.

--- spec/tropo2-ask_spec.rb
+++ spec/tropo2-ask_spec.rb
@@ -130,33 +130,8 @@ describe "Tropo2AutomatedFunctionalTesting" do
     
       @tropo2.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should eql true
     end
-  
-    it "Should timeout on an ask if a timeout is specified and get a NOINPUT" do
-      pending('https://github.com/tropo/tropo2/issues/24')
-      @tropo1.script_content = <<-SCRIPT_CONTENT
-        call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
-        wait #{@config['tropo1']['wait_to_hangup']}
-      SCRIPT_CONTENT
-      @tropo1.place_call @config['tropo1']['session_url']
-  
-      @tropo2.read_event_queue.should be_a_valid_call_event
-      @tropo2.answer.should eql true
     
-      time = Time.now
-      @tropo2.ask('Yeap', { :choices => 'yes, no', :timeout => 3000 }).should eql true
-      seconds_elapsed = Time.now - time
-      ap seconds_elapsed
-      ask_event = @tropo2.read_event_queue
-      ask_event.should be_a_valid_ask_event
-    
-      hangup_event = @tropo2.hangup
-      hangup_event.should be_a_valid_hangup_event
-    
-      @tropo2.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should eql true
-    end
-    
-    it "Should ask and get a STOP event" do
-      pending('https://github.com/tropo/punchblock/issues/24 & https://github.com/tropo/tropo2/issues/29')
+    it "Should ask and get a NOINPUT event" do
       @tropo1.script_content = <<-SCRIPT_CONTENT
         call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
         wait 5000
@@ -167,10 +142,53 @@ describe "Tropo2AutomatedFunctionalTesting" do
       @tropo2.read_event_queue.should be_a_valid_call_event
       @tropo2.answer.should eql true
     
-      @tropo2.ask('Yeap', { :choices => 'yes, no' })
+      @tropo2.ask('Yeap', { :choices => 'yes, no',
+                            :timeout => 2000 })
+      @tropo2.read_event_queue.should be_a_valid_noinput_event
+      @tropo2.read_event_queue.should be_a_valid_hangup_event
+      
+      @tropo2.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should eql true
+    end
+    
+    it "Should ask and get a NOMATCH event with min_confidence set to 1" do
+      pending('https://github.com/tropo/tropo2/issues/30')
+      @tropo1.script_content = <<-SCRIPT_CONTENT
+        call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
+        wait 1000
+        say 'elephant'
+        wait #{@config['tropo1']['wait_to_hangup']}
+        hangup
+      SCRIPT_CONTENT
+      @tropo1.place_call @config['tropo1']['session_url']
+  
+      @tropo2.read_event_queue.should be_a_valid_call_event
+      @tropo2.answer.should eql true
+    
+      @tropo2.ask('Yeap', { :choices        => 'red, green',
+                            :timeout        => 2000,
+                            :min_confidence => '1' })
+      @tropo2.read_event_queue.should be_a_valid_nomatch_event
+      @tropo2.read_event_queue.should be_a_valid_hangup_event
+      
+      @tropo2.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should eql true
+    end
+    
+    it "Should ask and get a STOP if the farside hangs up before the command complete" do
+      pending('https://github.com/tropo/tropo2/issues/32')
+      @tropo1.script_content = <<-SCRIPT_CONTENT
+        call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
+        wait 1000
+        hangup
+      SCRIPT_CONTENT
+      @tropo1.place_call @config['tropo1']['session_url']
+  
+      @tropo2.read_event_queue.should be_a_valid_call_event
+      @tropo2.answer.should eql true
+    
+      @tropo2.ask('Yeap', { :choices => 'red, green' })
       @tropo2.read_event_queue.should be_a_valid_stopped_ask_event
       @tropo2.read_event_queue.should be_a_valid_hangup_event
-    
+      
       @tropo2.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should eql true
     end
   end
