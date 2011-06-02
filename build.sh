77d065432d9e368b7574d9cec2d26127ed5b7b10
mpermar <mpermar@gmail.com>
2011-06-02 17:51:57 +0200

    didnt work

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
 rvm use ruby-1.8.7
-rake format=junit hudson > spec/reports/SPEC-FunctionalTests.xml
+rake format=junit hudson
