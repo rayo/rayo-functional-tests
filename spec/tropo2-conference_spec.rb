db6cf59fdb7670c48d2e6e8c088d2e83e04e995d
Jason Goecke <jason@goecke.net>
2011-06-03 22:07:50 -0700

    Have our first multi_call conference test working

--- spec/tropo2-conference_spec.rb
+++ spec/tropo2-conference_spec.rb
@@ -22,7 +22,34 @@ describe "Tropo2AutomatedFunctionalTesting" do
     end
   
     it "Should put two callers into a conference and then hangup" do
-      pending('The DSL needs to handle two calls at a time now, for the first time. Will add.')
+      @tropo1.script_content = <<-SCRIPT_CONTENT
+        call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
+        wait #{@config['tropo1']['wait_to_hangup']}
+      SCRIPT_CONTENT
+      @tropo1.place_call @config['tropo1']['session_url']
+      
+      call_1 = @tropo2.get_call
+      call_1.call_event.should be_a_valid_call_event      
+      call_1.answer.should eql true
+      call_1.conference('1234').should eql true
+      
+      @tropo1.place_call @config['tropo1']['session_url']
+      call_2 = @tropo2.get_call
+      call_2.call_event.should be_a_valid_call_event      
+      call_2.answer.should eql true
+      call_2.conference('1234').should eql true
+      
+      call_1.hangup.should eql true
+      call_1.next_event.should be_a_valid_conference_event
+      call_1.next_event.should be_a_valid_hangup_event
+    
+      call_1.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should eql true
+      
+      call_2.hangup.should eql true
+      call_2.next_event.should be_a_valid_conference_event
+      call_2.next_event.should be_a_valid_hangup_event
+    
+      call_2.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should eql true
     end
   
     it "Should put two callers into a conference, validate media and hangup" do
