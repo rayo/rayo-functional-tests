b495a2a0bc213baba564133d071d91cb0f0129aa
Ben Langfeld <ben@langfeld.me>
2011-07-22 21:44:34 +0100

    Make sure a reject test actually hangs up

--- spec/tropo2-reject_spec.rb
+++ spec/tropo2-reject_spec.rb
@@ -47,7 +47,6 @@ describe "Reject command" do
   end
 
   it "Should reject and raise an error due to an invalid reason" do
-    pending
     @tropo1.script_content = <<-TROPO_SCRIPT_CONTENT
       call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
       wait #{@config['tropo1']['wait_to_hangup']}
@@ -57,6 +56,7 @@ describe "Reject command" do
     call = @tropo2.get_call
     call.call_event.should be_a_valid_call_event
     lambda { call.reject :reason => :foobar }.should raise_error(ArgumentError)
+    call.reject(:reason => :busy).should be_true
 
     call.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should be_true
   end
