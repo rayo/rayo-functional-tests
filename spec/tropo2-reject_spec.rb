78a42f30f18531395db56bb79a58bb5f632d5bba
Ben Langfeld <ben@langfeld.me>
2011-07-22 21:29:20 +0100

    Fix a reject spec

--- spec/tropo2-reject_spec.rb
+++ spec/tropo2-reject_spec.rb
@@ -2,20 +2,18 @@ require 'spec_helper'
 
 describe "Reject command" do
   it "Should reject with a declined reason" do
-    pending 'https://github.com/tropo/tropo2/issues/81' do
-      @tropo1.script_content = <<-TROPO_SCRIPT_CONTENT
-        call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
-        wait #{@config['tropo1']['wait_to_hangup']}
-      TROPO_SCRIPT_CONTENT
-      @tropo1.place_call @config['tropo1']['session_url']
+    @tropo1.script_content = <<-TROPO_SCRIPT_CONTENT
+      call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
+      wait #{@config['tropo1']['wait_to_hangup']}
+    TROPO_SCRIPT_CONTENT
+    @tropo1.place_call @config['tropo1']['session_url']
 
-      call = @tropo2.get_call
-      call.call_event.should be_a_valid_call_event
-      call.reject(:reason => :declined).should be_true
-      call.next_event.should be_a_valid_reject_event
+    call = @tropo2.get_call
+    call.call_event.should be_a_valid_call_event
+    call.reject(:reason => :decline).should be_true
+    call.next_event.should be_a_valid_reject_event
 
-      call.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should be_true
-    end
+    call.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should be_true
   end
 
   it "Should reject with a busy reason" do
