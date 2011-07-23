72148fffef805e13c0582f87357aa6040c4af0cf
Ben Langfeld <ben@langfeld.me>
2011-07-23 15:18:33 +0100

    Fix a failing Say spec, testing actions

--- spec/tropo2-say_spec.rb
+++ spec/tropo2-say_spec.rb
@@ -42,7 +42,7 @@ describe "Say command" do
     call.call_event.should be_a_valid_call_event
     call.answer.should be_true
 
-    call.say(:audio => { :url => 'http://dl.dropbox.com/u/25511/Voxeo/troporocks.mp3' }).should be_true
+    call.say(:audio => { :url => @config['audio_url'] }).should be_true
 
     sleep 9 #Wait for audio file to complete playing
 
@@ -85,32 +85,31 @@ describe "Say command" do
   end
 
   it "Should say some audio, wait 2 seconds, pause, wait 2 seconds, resume, wait 2 seconds and then stop" do
-    pending 'https://github.com/tropo/punchblock/issues/10' do
-      @tropo1.script_content = <<-SCRIPT_CONTENT
-        call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
-        wait #{@config['tropo1']['wait_to_hangup']}
-      SCRIPT_CONTENT
-      @tropo1.place_call @config['tropo1']['session_url']
+    @tropo1.script_content = <<-SCRIPT_CONTENT
+      call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
+      wait #{@config['tropo1']['wait_to_hangup']}*2
+    SCRIPT_CONTENT
+    @tropo1.place_call @config['tropo1']['session_url']
 
-      call = @tropo2.get_call
-      call.call_event.should be_a_valid_call_event
-      call.answer.should be_true
+    call = @tropo2.get_call
+    call.call_event.should be_a_valid_call_event
+    call.answer.should be_true
 
-      say_event = call.say 'http://dl.dropbox.com/u/25511/Voxeo/troporocks.mp3', :url
-      ap say_event
+    say_command = call.say :audio => { :url => @config['audio_url'] }
 
-      sleep 2
-      ap say_event.pause
-      sleep 2
-      ap say_event.resume
-      sleep 2
-      ap say_event.stop
+    sleep 2
+    say_command.pause!
+    sleep 2
+    say_command.resume!
+    sleep 2
+    say_command.stop!
 
-      hangup_event = @tropo2.hangup(call_event)
-      hangup_event.should be_a_valid_hangup_event
+    call.next_event.should be_a_valid_stopped_say_event
 
-      @tropo2.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should be_true
-    end
+    call.hangup.should be_true
+    call.next_event.should be_a_valid_hangup_event
+
+    call.last_event?(@config['tropo2_queue']['last_stanza_timeout']).should be_true
   end
 
   it "Should say an audio URL and get a stop event" do
@@ -125,7 +124,7 @@ describe "Say command" do
     call.call_event.should be_a_valid_call_event
     call.answer.should be_true
 
-    call.say(:audio => { :url => 'http://dl.dropbox.com/u/25511/Voxeo/troporocks.mp3' }).should be_true
+    call.say(:audio => { :url => @config['audio_url'] }).should be_true
 
     call.next_event.should be_a_valid_complete_hangup_event
     call.next_event.should be_a_valid_hangup_event
