f564f80a58b859616651ff8f4c9559b05defbbb8
Ben Langfeld <ben@langfeld.me>
2011-07-27 11:00:30 +0100

    Move over to new namespaces

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@
   drb
   logger
   punchblock
-  rspec-tropo2
+  rspec-rayo
 }.each { |lib| require lib }
 
 Thread.abort_on_exception = true
@@ -21,12 +21,12 @@ RSpec.configure do |config|
 
     random_jid = "user#{(1..1000).to_a.sort_by { rand }.first}@127.0.0.1"
 
-    @tropo2 = Tropo2Utilities::Tropo2Driver.new :username         => ENV['TROPO2_JID'] || @config['tropo2_server']['jid'] || random_jid,
-                                                :password         => ENV['TROPO2_PASSWORD'] || @config['tropo2_server']['password'],
-                                                :wire_logger      => Logger.new(@config['tropo2_server']['wire_log']),
-                                                :transport_logger => Logger.new(@config['tropo2_server']['transport_log']),
-                                                :log_level        => Logger::DEBUG,
-                                                :queue_timeout    => @config['tropo2_queue']['connection_timeout']
+    @tropo2 = RSpecRayo::RayoDriver.new :username         => ENV['TROPO2_JID'] || @config['tropo2_server']['jid'] || random_jid,
+                                        :password         => ENV['TROPO2_PASSWORD'] || @config['tropo2_server']['password'],
+                                        :wire_logger      => Logger.new(@config['tropo2_server']['wire_log']),
+                                        :transport_logger => Logger.new(@config['tropo2_server']['transport_log']),
+                                        :log_level        => Logger::DEBUG,
+                                        :queue_timeout    => @config['tropo2_queue']['connection_timeout']
 
     @config['tropo2_server']['sip_uri'] ||= ENV['TROPO2_SIP_URI'] || random_jid
 
@@ -36,7 +36,7 @@ RSpec.configure do |config|
 
     @drb_server_uri = "druby://#{drb_server_host_and_port}"
 
-    @tropo1 = Tropo2Utilities::Tropo1Driver.new @drb_server_uri, @config['tropo1']['latch_timeout']
+    @tropo1 = RSpecRayo::Tropo1Driver.new @drb_server_uri, @config['tropo1']['latch_timeout']
 
     status = @tropo2.read_queue(@tropo2.event_queue)
     abort 'Could not connect to Prism XMPP Server. Aborting!' if status != 'CONNECTED'
