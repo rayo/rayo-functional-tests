7b194ec9b925a6694bc06606d1b42821542c3c57
Ben Langfeld <ben@langfeld.me>
2011-07-27 16:42:48 +0100

    Clean up CDR tests a little and print CDR to the console for debugging

--- spec/tropo2-cdr_spec.rb
+++ spec/tropo2-cdr_spec.rb
@@ -5,45 +5,46 @@ require 'json'
 describe "CDR Manager" do
 
   it "Should create a CDR for an incoming call" do
-    @tropo1.script_content = <<-TROPO_SCRIPT_CONTENT
-      call 'sip:' + '#{@config['tropo2_server']['sip_uri']}'
-      wait #{@config['tropo1']['wait_to_hangup']}
+    place_call_with_script <<-TROPO_SCRIPT_CONTENT
+      call_tropo2
+      wait_to_hangup
     TROPO_SCRIPT_CONTENT
-    @tropo1.place_call @config['tropo1']['session_url']
 
-	get_call_and_answer
-  
+    get_call_and_answer
+
     server = @config['tropo2_server']['server']
-    port = @config['tropo2_server']['port'].to_i  
-    res = Net::HTTP.get_response(server, '/tropo2/jmx/read/com.tropo:Type=Cdrs/ActiveCDRs', port)    
-    json = JSON.parse res.body
-    activeCdrs = json['value']
-    
-	activeCdrs[activeCdrs.length-1]['callId'].should eql @call.call_event.call_id
-    
-    hangup_and_confirm
-  end
-  
-  it "Should create a CDR for an outgoing call" do
-    @tropo1.script_content = <<-TROPO_SCRIPT_CONTENT
-      accept      
-      wait #{@config['tropo1']['wait_to_hangup']}
-      hangup
-    TROPO_SCRIPT_CONTENT
-    
-    @call = @tropo2.dial :to      => @config['tropo1']['call_destination'],
-                         :from    => 'tel:+14155551212',
-                         :headers => { 'x-tropo2-drb-address' => @drb_server_uri }
-    
-    server = @config['tropo2_server']['server']
-    port = @config['tropo2_server']['port'].to_i  
+    port = @config['tropo2_server']['port'].to_i
     res = Net::HTTP.get_response(server, '/tropo2/jmx/read/com.tropo:Type=Cdrs/ActiveCDRs', port)
     json = JSON.parse res.body
     activeCdrs = json['value']
-    
-	activeCdrs[activeCdrs.length-1]['callId'].should eql @call.call_event.call_id
+
+    activeCdrs.last['callId'].should eql @call.call_event.call_id
+
+    hangup_and_confirm
+  end
+
+  it "Should create a CDR for an outgoing call" do
+    @tropo1.script_content = <<-TROPO_SCRIPT_CONTENT
+      accept
+      wait_to_hangup
+      hangup
+    TROPO_SCRIPT_CONTENT
+
+    @call = @tropo2.dial :to      => @config['tropo1']['call_destination'],
+                         :from    => 'tel:+14155551212',
+                         :headers => { 'x-tropo2-drb-address' => @drb_server_uri }
+
+    server = @config['tropo2_server']['server']
+    port = @config['tropo2_server']['port'].to_i
+    res = Net::HTTP.get_response(server, '/tropo2/jmx/read/com.tropo:Type=Cdrs/ActiveCDRs', port)
+    json = JSON.parse res.body
+    activeCdrs = json['value']
+
+    p activeCdrs
+
+    activeCdrs.last['callId'].should eql @call.call_event.call_id
 
     @call.ring_event.should be_a_valid_ringing_event
-    @call.next_event.should be_a_valid_reject_event    
+    @call.next_event.should be_a_valid_reject_event
   end
 end
