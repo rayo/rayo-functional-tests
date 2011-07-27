ee4557b75c58bc439c7dcef1449dd11b3a30c7b9
mpermar <mpermar@gmail.com>
2011-07-27 17:53:33 +0200

    trying with a single test

--- spec/tropo2-cdr_spec.rb
+++ spec/tropo2-cdr_spec.rb
@@ -4,25 +4,6 @@ require 'json'
 
 describe "CDR Manager" do
 
-  it "Should create a CDR for an incoming call" do
-    place_call_with_script <<-TROPO_SCRIPT_CONTENT
-      call_tropo2
-      wait_to_hangup
-    TROPO_SCRIPT_CONTENT
-
-    get_call_and_answer
-
-    server = @config['tropo2_server']['server']
-    port = @config['tropo2_server']['port'].to_i
-    res = Net::HTTP.get_response(server, '/tropo2/jmx/read/com.tropo:Type=Cdrs/ActiveCDRs', port)
-    json = JSON.parse res.body
-    activeCdrs = json['value']
-
-    activeCdrs.last['callId'].should eql @call.call_event.call_id
-
-    hangup_and_confirm
-  end
-
   it "Should create a CDR for an outgoing call" do
     @tropo1.script_content = <<-TROPO_SCRIPT_CONTENT
       accept
@@ -41,6 +22,7 @@ describe "CDR Manager" do
     activeCdrs = json['value']
 
     p activeCdrs
+    p @call.call_event.call_id
 
     activeCdrs.last['callId'].should eql @call.call_event.call_id
 
