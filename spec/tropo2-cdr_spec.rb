bf09f0b6e0d0b3a6a1768cbf8de5a58878f9ec8b
mpermar <mpermar@gmail.com>
2011-07-28 11:27:57 +0200

    Added a new functional test that validates that content is appended to CDRs

--- spec/tropo2-cdr_spec.rb
+++ spec/tropo2-cdr_spec.rb
@@ -58,4 +58,46 @@ describe "CDR Manager" do
     @call.ring_event.should be_a_valid_ringing_event
     @call.next_event.should be_a_valid_reject_event
   end
+  
+  it "Should create a CDR with transcript with all actions" do
+    @tropo1.add_latch :responded
+
+    place_call_with_script <<-SCRIPT_CONTENT
+      call_tropo2
+      ask 'One7', :choices     => 'yes, no',
+                 :onBadChoice => lambda { ozone_testing_server.result = 'badchoice' },
+                 :onChoice    => lambda { |event| ozone_testing_server.result = event.value  }
+      trigger_latch :responded
+      wait_to_hangup
+    SCRIPT_CONTENT
+
+    get_call_and_answer
+
+    @call.say(:text => 'yes').should be_true
+
+    @tropo1.wait :responded
+
+    @call.next_event.should be_a_valid_say_event
+
+    server = @config['tropo2_server']['server']
+    port = @config['tropo2_server']['port'].to_i
+    res = Net::HTTP.get_response(server, '/tropo2/jmx/read/com.tropo:Type=Cdrs/ActiveCDRs', port)
+    json = JSON.parse res.body
+    activeCdrs = json['value']
+
+    activeCdrs.first['callId'].should eql @call.call_event.call_id
+    transcript = activeCdrs.first['transcript'].to_s
+
+	true.should eql transcript.include? "<offer"
+	true.should eql transcript.include? "<answer"
+	true.should eql transcript.include? "<say"
+	true.should eql transcript.include? "<complete"
+	true.should eql transcript.include? "<success"
+	    
+    hangup_and_confirm
+
+    @tropo1.result.should eql 'yes'
+  end  
+  
+  
 end
