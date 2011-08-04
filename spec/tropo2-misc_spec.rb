34e5acc15df6be69e25064b5d3d35b9a8a4d0340
Ben Langfeld <ben@langfeld.me>
2011-08-04 15:52:21 +0100

    Add tests for mute/unmute commands

---
+++ spec/tropo2-misc_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "Misc commands" do
+  it "can mute & unmute the call", :focus => true do
+    place_call_with_script <<-SCRIPT_CONTENT
+      call_tropo2
+      wait_to_hangup
+    SCRIPT_CONTENT
+
+    get_call_and_answer
+
+    @call.mute.should be_true
+    @call.unmute.should be_true
+    hangup_and_confirm
+  end
+end
