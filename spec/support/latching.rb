05c7bf6f14d86f8409978094a9c0d50287266e21
Ben Langfeld <ben@langfeld.me>
2011-08-11 16:10:18 +0100

    Add an improved timeout matcher to latching which reports the call ID

--- spec/support/latching.rb
+++ spec/support/latching.rb
@@ -3,9 +3,31 @@ def add_latch(*latch_names)
 end
 
 def wait_on_latch(latch_name)
-  @tropo1.wait(latch_name).should == true
+  @tropo1.wait(latch_name).should_not timeout
 end
 
 def trigger_latch(latch_name)
   @tropo1.trigger latch_name
+end
+
+RSpec::Matchers.define :timeout do
+  match do |actual|
+    actual.should == false
+  end
+
+  failure_message_for_should do |actual|
+    "expected latch to timeout.".tap do |s|
+      s << "call_id: #{@call.call_id}" if @call
+    end
+  end
+
+  failure_message_for_should do |actual|
+    "expected latch not to timeout.".tap do |s|
+      s << "call_id: #{@call.call_id}" if @call
+    end
+  end
+
+  description do
+    "timeout"
+  end
 end
