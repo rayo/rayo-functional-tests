5becaf90622fa1c44334571ca36a175fc77525e9
Ben Langfeld <ben@langfeld.me>
2011-09-22 13:02:27 +0100

    Fix multi-dial timeout issue

--- spec/rayo-dial_spec.rb
+++ spec/rayo-dial_spec.rb
@@ -56,12 +56,11 @@ describe "Dial command" do
     TROPO_SCRIPT_CONTENT
 
     call1 = @rayo.dial(tropo1_dial_options).should have_dialed_correctly
-    call2 = @rayo.dial(tropo1_dial_options).should have_dialed_correctly
-
     call1.next_event.should be_a_valid_answered_event
     call1.next_event.should be_a_valid_hangup_event
     call1.last_event?(@config['rayo_queue']['last_stanza_timeout']).should == true
 
+    call2 = @rayo.dial(tropo1_dial_options).should have_dialed_correctly
     call2.next_event.should be_a_valid_answered_event
     call2.next_event.should be_a_valid_hangup_event
     call2.last_event?(@config['rayo_queue']['last_stanza_timeout']).should == true
