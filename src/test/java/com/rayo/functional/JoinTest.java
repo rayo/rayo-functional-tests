f115cf3bcb9c7e72b066cb06fba2e4d99d52ab1a
mpermar <mpermar@gmail.com>
2011-10-27 12:06:44 +0200

    extra tests

--- src/test/java/com/rayo/functional/JoinTest.java
+++ src/test/java/com/rayo/functional/JoinTest.java
@@ -6,29 +6,63 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import com.rayo.functional.base.MohoBasedIntegrationTest;
+import com.voxeo.moho.Call;
 import com.voxeo.moho.IncomingCall;
 import com.voxeo.moho.OutgoingCall;
 import com.voxeo.moho.Participant.JoinType;
+import com.voxeo.moho.common.event.MohoInputCompleteEvent;
 import com.voxeo.moho.common.event.MohoJoinCompleteEvent;
+import com.voxeo.moho.media.Input;
+import com.voxeo.moho.media.input.InputCommand;
+import com.voxeo.moho.media.input.SimpleGrammar;
 
 public class JoinTest extends MohoBasedIntegrationTest {
 
 	@Test
-	@Ignore
-	public void testJoin() {
+	public void testJoinBridge() {
 		
 	    OutgoingCall outgoing1 = dial();	    	    
 	    IncomingCall incoming1 = getIncomingCall();
-	    System.out.println("***" + incoming1);
 	    incoming1.answer();
 	    
 	    OutgoingCall outgoing2 = dial();
 	    IncomingCall incoming2 = getIncomingCall();
-	    System.out.println("***" + incoming2);
 	    incoming2.join(incoming1, JoinType.BRIDGE, Direction.DUPLEX);
-	    
 	    
 	    assertReceived(MohoJoinCompleteEvent.class, incoming1);
 	    assertReceived(MohoJoinCompleteEvent.class, incoming2);
+	    
+	    Input<Call> input1 = incoming1.input(new InputCommand(new SimpleGrammar("yes,no")));
+	    incoming2.output("yes");
+	    outgoing1.output("yes");
+	    outgoing2.output("yes");
+
+	    // Asserting, but is not received
+	    assertReceived(MohoInputCompleteEvent.class, incoming1);
+	}
+	
+	@Test
+	@Ignore
+	public void testJoinBridgeFails() {
+		
+	    OutgoingCall outgoing1 = dial();	    	    
+	    IncomingCall incoming1 = getIncomingCall();
+	    incoming1.answer();
+	    
+	    OutgoingCall outgoing2 = dial();
+	    incoming1.join(outgoing2, JoinType.BRIDGE, Direction.DUPLEX);
+	    	    	    
+	    assertReceived(MohoJoinCompleteEvent.class, incoming1);
+	    
+	    Input<Call> input1 = outgoing1.input(new InputCommand(new SimpleGrammar("yes,no")));
+	    Input<Call> input2 = outgoing2.input(new InputCommand(new SimpleGrammar("yes,no")));
+	    incoming1.output("yes");
+	    
+	    try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
