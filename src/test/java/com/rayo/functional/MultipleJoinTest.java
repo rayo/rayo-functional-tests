615aabaf62dc8a7c7e08ac268968dd459648fe2b
mpermar <mpermar@gmail.com>
2011-10-31 15:08:23 +0100

    more tests

--- src/test/java/com/rayo/functional/MultipleJoinTest.java
+++ src/test/java/com/rayo/functional/MultipleJoinTest.java
@@ -1,6 +1,6 @@
 package com.rayo.functional;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import javax.media.mscontrol.join.Joinable.Direction;
 
@@ -12,7 +12,10 @@ import com.voxeo.moho.Call;
 import com.voxeo.moho.IncomingCall;
 import com.voxeo.moho.OutgoingCall;
 import com.voxeo.moho.Participant.JoinType;
+import com.voxeo.moho.event.CallCompleteEvent;
 import com.voxeo.moho.event.InputCompleteEvent;
+import com.voxeo.moho.event.JoinCompleteEvent;
+import com.voxeo.moho.event.OutputCompleteEvent;
 import com.voxeo.moho.media.Input;
 import com.voxeo.moho.media.input.InputCommand;
 import com.voxeo.moho.media.input.SimpleGrammar;
@@ -20,6 +23,7 @@ import com.voxeo.moho.media.input.SimpleGrammar;
 public class MultipleJoinTest extends MohoBasedIntegrationTest {
 
 	@Test
+	@Ignore
 	public void testOutputIsReceivedByMultipleCalls() {
 		
 	    OutgoingCall outgoing1 = dial();	    
@@ -48,5 +52,70 @@ public class MultipleJoinTest extends MohoBasedIntegrationTest {
 
 	    assertReceived(InputCompleteEvent.class, input2);
 	    assertReceived(InputCompleteEvent.class, input3);
+	}
+	
+
+	@Test
+	public void testJoinDirectModeFailsIfJoined() {
+		
+	    OutgoingCall outgoing1 = dial();	    
+	    IncomingCall incoming1 = getIncomingCall();
+	    assertNotNull(incoming1);
+	    incoming1.answer();
+
+	    OutgoingCall outgoing2 =dial();
+	    IncomingCall incoming2 = getIncomingCall();
+	    assertNotNull(incoming2);
+	    incoming2.answer();
+
+	    OutgoingCall outgoing3 =dial();
+	    IncomingCall incoming3 = getIncomingCall();
+	    assertNotNull(incoming3);
+	    incoming3.answer();
+	    waitForEvents();
+
+	    incoming1.join(incoming2, JoinType.DIRECT, false, Direction.DUPLEX);
+	    waitForEvents();
+	    assertReceived(JoinCompleteEvent.class, incoming1);
+	    assertReceived(JoinCompleteEvent.class, incoming2);
+
+	    try {
+	    	incoming3.join(incoming1, JoinType.DIRECT, false, Direction.DUPLEX);
+	    } catch (Exception e) {
+	    	// Moho should have better xmpp error handling
+	    	assertTrue(e.getMessage().contains("is already joined"));
+	    }
+	    assertReceived(CallCompleteEvent.class, incoming3);
+	    assertReceived(CallCompleteEvent.class, outgoing3);
+	    
+	    // Assert that incoming1 keeps working
+	    Input<Call> input = incoming2.input("yes,no");
+	    incoming1.output("yes");
+	    waitForEvents();
+	    assertReceived(InputCompleteEvent.class, input);	    
+	    
+	    outgoing1.hangup();
+	    outgoing2.hangup();
+	    waitForEvents();
+	}
+	
+	@Test
+	@Ignore // Ask Willie
+	public void testJoinDirectModeFailsIfJoined2() {
+		
+	    OutgoingCall outgoing1 = dial();	    
+	    waitForEvents();
+	    OutgoingCall outgoing2 = dial();
+	    waitForEvents();
+	    
+	    outgoing1.join(outgoing2, JoinType.DIRECT, false, Direction.DUPLEX);
+	    waitForEvents();
+	    
+	    assertReceived(JoinCompleteEvent.class, outgoing1);
+	    assertReceived(JoinCompleteEvent.class, outgoing2);
+	    
+	    outgoing1.hangup();
+	    outgoing2.hangup();
+	    waitForEvents();
 	}
 }
