0b3f4f94c3ee5fe8caf29cbd6a76acb97e93585f
mpermar <mpermar@gmail.com>
2012-01-09 11:08:24 +0100

    new reject tests.

---
+++ src/test/java/com/rayo/functional/rayoapi/RayoRejectTest.java
@@ -0,0 +1,67 @@
+package com.rayo.functional.rayoapi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.rayo.client.RayoClient;
+import com.rayo.client.XmppConnection;
+import com.rayo.client.xmpp.extensions.Extension;
+import com.rayo.client.xmpp.stanza.IQ;
+import com.rayo.client.xmpp.stanza.Error.Condition;
+import com.rayo.core.EndEvent;
+import com.rayo.core.EndEvent.Reason;
+import com.rayo.core.RejectCommand;
+import com.rayo.core.validation.Messages;
+import com.rayo.functional.base.RayoBasedIntegrationTest;
+
+public class RayoRejectTest extends RayoBasedIntegrationTest {
+
+	@Test
+	public void testEmptyReject() throws Exception {
+		
+		String outgoing = dial().getCallId();
+		try {
+			String incomingCallId = getIncomingCall().getCallId();			
+			rayoClient.reject(incomingCallId);
+			EndEvent end = assertReceived(EndEvent.class,incomingCallId);
+			assertEquals(end.getReason(), Reason.REJECT);
+		} finally {
+			rayoClient.hangup(outgoing);
+		}
+	}
+	
+
+	@Test
+	public void testRejectWithNoReason() throws Exception {
+		
+		String outgoing = dial().getCallId();
+
+		try {
+	    	String incomingCallId = getIncomingCall().getCallId();   
+		    assertNotNull(incomingCallId);
+
+		    RejectCommand reject = new RejectCommand();
+		    reject.setReason(null);
+		    XmppConnection connection = rayoClient.getXmppConnection();
+		    IQ iq = new IQ(IQ.Type.get)
+				.setFrom(buildFrom(connection))
+				.setTo(incomingCallId+"@"+rayoServer+"/"+RayoClient.DEFAULT_RESOURCE)
+				.setChild(Extension.create(reject));
+		    IQ result = (IQ)connection.sendAndWait(iq);		    
+		    assertTrue(result.isError());
+		    assertEquals(result.getError().getCondition(), Condition.bad_request);
+		    assertTrue(result.getError().getText().contains(Messages.MISSING_REASON));
+		} finally {
+			rayoClient.hangup(outgoing);
+		}
+	}
+	
+	
+	private String buildFrom(XmppConnection connection) {
+		
+		return connection.getUsername() + "@" + connection.getServiceName() + "/" + connection.getResource();
+	}
+}
