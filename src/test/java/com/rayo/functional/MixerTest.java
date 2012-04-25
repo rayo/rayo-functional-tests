fcf5ce320a1ef82c9255b0692ec5fbc36660d16e
mpermar <mpermar@gmail.com>
2012-04-25 09:08:24 +0200

    Use a random mixer name instead of "1234" as currently we are running the tests on the same server, sometimes without restarting it so the mixer can still be there.

--- src/test/java/com/rayo/functional/MixerTest.java
+++ src/test/java/com/rayo/functional/MixerTest.java
@@ -1,10 +1,10 @@
 package com.rayo.functional;
 
+import java.util.UUID;
+
 import javax.media.mscontrol.join.Joinable.Direction;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.rayo.functional.base.MohoBasedIntegrationTest;
 import com.voxeo.moho.IncomingCall;
@@ -17,90 +17,90 @@ import com.voxeo.moho.event.ActiveSpeakerEvent;
 import com.voxeo.moho.event.InputCompleteEvent;
 
 public class MixerTest extends MohoBasedIntegrationTest {
-  private Logger log = LoggerFactory.getLogger(MixerTest.class);
 
-  // test https://voxeolabs.atlassian.net/browse/RAYO-4
-  @Test
-  public void testJoinSpeak() {
-    OutgoingCall outgoing1 = null;
-    OutgoingCall outgoing2 = null;
+	// test https://voxeolabs.atlassian.net/browse/RAYO-4
+	@Test
+	public void testJoinSpeak() {
 
-    Mixer mixer = null;
-    outgoing1 = dial();
-    IncomingCall incoming1 = getIncomingCall();
-    incoming1.answer();
-    waitForEvents();
+		String mixerName = UUID.randomUUID().toString();
+		OutgoingCall outgoing1 = null;
+		OutgoingCall outgoing2 = null;
 
-    outgoing2 = dial();
-    IncomingCall incoming2 = getIncomingCall();
-    incoming2.answer();
-    waitForEvents();
+		Mixer mixer = null;
+		outgoing1 = dial();
+		IncomingCall incoming1 = getIncomingCall();
+		incoming1.answer();
+		waitForEvents();
 
-    // create mixer
-    MixerEndpoint mixerEndpoint = mohoRemote.createMixerEndpoint();
-    mixer = mixerEndpoint.create("1234", null);
+		outgoing2 = dial();
+		IncomingCall incoming2 = getIncomingCall();
+		incoming2.answer();
+		waitForEvents();
 
-    Joint joint = incoming1.join(mixer, JoinType.BRIDGE, Direction.DUPLEX);
+		// create mixer
+		MixerEndpoint mixerEndpoint = mohoRemote.createMixerEndpoint();
+		mixer = mixerEndpoint.create(mixerName, null);
 
-    try {
-      joint.get();
-    }
-    catch (Exception ex) {
-      ex.printStackTrace();
-    }
+		Joint joint = incoming1.join(mixer, JoinType.BRIDGE, Direction.DUPLEX);
 
-    outgoing1.input("yes,no");
-    mixer.output("yes");
-    waitForEvents(4000);
-    assertReceived(InputCompleteEvent.class, outgoing1);
+		try {
+			joint.get();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-    Joint joint2 = incoming2.join(mixer, JoinType.BRIDGE, Direction.DUPLEX);
+		outgoing1.input("yes,no");
+		mixer.output("yes");
+		waitForEvents(4000);
+		assertReceived(InputCompleteEvent.class, outgoing1);
 
-    try {
-      joint2.get();
-    }
-    catch (Exception ex) {
-      ex.printStackTrace();
-    }
+		Joint joint2 = incoming2.join(mixer, JoinType.BRIDGE, Direction.DUPLEX);
 
-    outgoing1.input("yes,no");
-    outgoing2.input("yes,no");
-    mixer.output("yes");
-    waitForEvents(4000);
-    assertReceived(InputCompleteEvent.class, outgoing1);
-    assertReceived(InputCompleteEvent.class, outgoing2);
+		try {
+			joint2.get();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-    outgoing1.hangup();
-    outgoing2.hangup();
-    mixer.disconnect();
-    waitForEvents();
-  }
+		outgoing1.input("yes,no");
+		outgoing2.input("yes,no");
+		mixer.output("yes");
+		waitForEvents(4000);
+		assertReceived(InputCompleteEvent.class, outgoing1);
+		assertReceived(InputCompleteEvent.class, outgoing2);
 
-  @Test
-  public void testActiveSpeakerEvent() {
-    OutgoingCall outgoing1 = dial();
-    IncomingCall incoming1 = getIncomingCall();
-    incoming1.answer();
-    waitForEvents();
+		outgoing1.hangup();
+		outgoing2.hangup();
+		mixer.disconnect();
+		waitForEvents();
+	}
 
-    // create mixer
-    Mixer mixer = createMixer("1234");
+	@Test
+	public void testActiveSpeakerEvent() {
 
-    Joint joint = incoming1.join(mixer, JoinType.BRIDGE, Direction.DUPLEX);
+		String mixerName = UUID.randomUUID().toString();
+		OutgoingCall outgoing1 = dial();
+		IncomingCall incoming1 = getIncomingCall();
+		incoming1.answer();
+		waitForEvents();
 
-    try {
-      joint.get();
-    }
-    catch (Exception ex) {
-      ex.printStackTrace();
-    }
+		// create mixer
+		Mixer mixer = createMixer(mixerName);
 
-    outgoing1.output("yes");
-    waitForEvents(4000);
-    assertReceived(ActiveSpeakerEvent.class, mixer);
+		Joint joint = incoming1.join(mixer, JoinType.BRIDGE, Direction.DUPLEX);
 
-    outgoing1.hangup();
-    mixer.disconnect();
-    waitForEvents();
-  }
+		try {
+			joint.get();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		outgoing1.output("yes");
+		waitForEvents(4000);
+		assertReceived(ActiveSpeakerEvent.class, mixer);
+
+		outgoing1.hangup();
+		mixer.disconnect();
+		waitForEvents();
+	}
 }
