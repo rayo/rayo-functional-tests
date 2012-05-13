7b533e1e4ed50e56b07fb702f97df1a549aeb2a1
John Dyer <johntdyer@gmail.com>
2012-05-12 22:28:29 -0400

    changed rayo server parameter per Martins request

--- start_tests.sh
+++ start_tests.sh
@@ -147,9 +147,8 @@ wait
 #  Run tests
 #
 ##########################################
-
 PARAMETERS="-Dxmpp.server=${XMPP_SERVER_DOMAIN}"
-PARAMETERS="${PARAMETERS} -Drayo.server=${GATEWAY_HOSTNAME_EXTERNAL}"
+PARAMETERS="${PARAMETERS} -Drayo.server=${rft-gw-client}.${TEST_DOMAIN}"
 PARAMETERS="${PARAMETERS} -Dxmpp.username=${XMPP_SERVER_USERNAME}"
 PARAMETERS="${PARAMETERS} -Dxmpp.password=${XMPP_SERVER_PASSWORD}"
 PARAMETERS="${PARAMETERS} -Dhudson.append.ext=true"
