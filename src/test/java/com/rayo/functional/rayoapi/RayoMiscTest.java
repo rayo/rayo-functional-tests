80e559679e323d7729fff247157ac0fc1a1d08d8
mpermar <mpermar@voxeolabs.net>
2013-05-24 09:13:24 +0200

    Splitted misc test into node/cluster tests.

--- src/test/java/com/rayo/functional/rayoapi/RayoMiscTest.java
+++ src/test/java/com/rayo/functional/rayoapi/RayoMiscTest.java
@@ -2,14 +2,9 @@ package com.rayo.functional.rayoapi;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.voxeo.rayo.client.XmppConnection;
-import com.voxeo.rayo.client.xmpp.stanza.Error.Condition;
-import com.voxeo.rayo.client.xmpp.stanza.IQ;
-import com.voxeo.rayo.client.xmpp.stanza.Ping;
 import com.rayo.functional.base.RayoBasedIntegrationTest;
 
 public class RayoMiscTest extends RayoBasedIntegrationTest {
@@ -46,50 +41,5 @@ public class RayoMiscTest extends RayoBasedIntegrationTest {
     	assertTrue(error.getText().contains("Call has not been answered"));
 
     	rayoClient.hangup(outgoingCallId);
-	}
-	
-	@Test
-	public void testSendCommandToGatewayWithInvalidAddress() throws Exception {
-
-		String outgoingCallId = dial().getCallId();
-	    
-    	String incomingCallId = getIncomingCall().getCallId();   
-	    assertNotNull(incomingCallId);
-
-	    XmppConnection connection = rayoClient.getXmppConnection();
-		IQ iq = new IQ(IQ.Type.get)
-			.setFrom(buildFrom(connection))
-			.setTo("crapurl@")
-			.setChild(new Ping());
-	    IQ result = (IQ)connection.sendAndWait(iq);
-	    assertTrue(result.isError());
-	    assertEquals(result.getError().getCondition(),Condition.jid_malformed);
-	    
-    	rayoClient.hangup(outgoingCallId);
-	}
-	
-	@Test
-	public void testSendCommandToRayoNodeWithInvalidAddress() throws Exception {
-
-		String outgoingCallId = dial().getCallId();
-	    
-    	String incomingCallId = getIncomingCall().getCallId();   
-	    assertNotNull(incomingCallId);
-
-	    XmppConnection connection = rayoClient.getXmppConnection();
-		IQ iq = new IQ(IQ.Type.get)
-			.setFrom(buildFrom(connection))
-			.setTo("crapurl@")
-			.setChild(new Ping());
-	    IQ result = (IQ)connection.sendAndWait(iq);
-	    assertTrue(result.isError());
-	    assertEquals(result.getError().getCondition(),Condition.jid_malformed);
-	    
-    	rayoClient.hangup(outgoingCallId);
-	}
-	
-	private String buildFrom(XmppConnection connection) {
-		
-		return connection.getUsername() + "@" + connection.getServiceName() + "/" + connection.getResource();
 	}
 }
